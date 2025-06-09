803e231891bcf78b62310d7ca4982519280df3ea
Reprator <mr.vikram004@gmail.com>
2025-06-10 01:10:04 +0530

    layout in details

--- Compose_Learning/app/src/main/java/dev/reprator/composelearning/MainActivity.kt
+++ Compose_Learning/app/src/main/java/dev/reprator/composelearning/MainActivity.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.SubcomposeLayout
 import androidx.compose.ui.tooling.preview.Preview
 import dev.reprator.composelearning.ui.theme.ComposeLearningTheme
 
@@ -20,16 +21,48 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeLearningTheme {
                 Scaffold( modifier = Modifier.fillMaxSize() ) { innerPadding ->
-                    Greeting(
+                    /*Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
-                    )
+                    )*/
+                    MySubcomposeLayout( modifier = Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
 
+@Composable
+fun MySubcomposeLayout(modifier: Modifier = Modifier) {
+    SubcomposeLayout { constraints ->
+
+        val topMeasurables = subcompose("top") {
+            Text("I'm at the top")
+        }
+
+        val topPlaceables = topMeasurables.map { it.measure(constraints) }
+        val topHeight = topPlaceables.maxOf { it.height }
+
+        val bottomMeasurables = subcompose("bottom") {
+            Text("I'm below top")
+        }
+        val bottomPlaceables = bottomMeasurables.map { it.measure(constraints) }
+
+        val totalHeight = topHeight + bottomPlaceables.maxOf { it.height }+220
+
+        layout(constraints.maxWidth, totalHeight) {
+            var y = 220
+            topPlaceables.forEach {
+                it.placeRelative(0, y)
+                y += it.height
+            }
+
+            bottomPlaceables.forEach {
+                it.placeRelative(0, y)
+            }
+        }
+    }
+}
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
